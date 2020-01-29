600879ac494d1cd54d76b82ae93ec72b7f9c5f80
Dylan Portmann <dylan.portmann@icloud.com>
2020-01-29 13:32:00 +0100

    Update Connexion.php

--- Connexion.php
+++ Connexion.php
@@ -99,14 +99,14 @@
                         <h2 class="sr-only">Login Form</h2><a class="forgot" href="perdu.html" style="color: rgb(186,0,0);font-size: 26px;">Wrong password or email</a></form>
                 </div>
                 <nav class="navbar navbar-light navbar-expand-md navbar navbar-expand-lg fixed-top" id="mainNav" style="color: rgb(255,255,255);font-size: 20px;font-family: Cabin, sans-serif;background-color: rgba(0,0,0,0.11);">
-                    <div class="container-fluid"><a class="navbar-brand" href="index.html" style="color: rgba(255,255,255,0.9);background-image: url(&quot;assets/img/logo_MautoRS_final.png&quot;);width: 65px;height: 65px;background-position: center;background-size: contain;"></a><button data-toggle="collapse"
+                    <div class="container-fluid"><a class="navbar-brand" href="index.php" style="color: rgba(255,255,255,0.9);background-image: url(&quot;assets/img/logo_MautoRS_final.png&quot;);width: 65px;height: 65px;background-position: center;background-size: contain;"></a><button data-toggle="collapse"
                             class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse" id="navcol-1" style="max-width: 328px;">
                             <ul class="nav navbar-nav">
-                                <li class="nav-item" role="presentation"><a class="nav-link active" href="event.html" style="color: rgba(255,255,255,0.9);">Evénement</a></li>
-                                <li class="nav-item" role="presentation"><a class="nav-link" href="media.html" style="color: rgba(255,255,255,0.5);">Média</a></li>
-                                <li class="nav-item" role="presentation"><a class="nav-link" href="shop.html" style="color: rgba(255,255,255,0.5);">Shop</a></li>
-                                <li class="nav-item" role="presentation"><a class="nav-link" href="Connexion.html" style="color: rgba(255,255,255,0.5);">Compte</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link active" href="event.php" style="color: rgba(255,255,255,0.9);">Evénement</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="media.php" style="color: rgba(255,255,255,0.5);">Média</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="shop.php" style="color: rgba(255,255,255,0.5);">Shop</a></li>
+                                <li class="nav-item" role="presentation"><a class="nav-link" href="Connexion.php" style="color: rgba(255,255,255,0.5);">Compte</a></li>
                             </ul>
                         </div>
                     </div>
