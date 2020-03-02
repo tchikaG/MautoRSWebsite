6f07316dd591db9e391a3268415cc1d7def82d08
Dylan Portmann <dylan.portmann@hotmail.com>
2020-03-02 13:14:40 +0100

    new

--- event.php
+++ event.php
@@ -25,14 +25,16 @@ include 'menu.php';
         </div>
     </div>
     <div style="background-color: rgb(238,244,247);">
-        <div class="container" style="padding-top: 50px;padding-bottom: 80px;"><button class="btn btn-primary" type="button" style="margin-left: 1006px;margin-bottom: 28px;background-color: rgba(93,130,169,0.83);font-size: 25px;">ADD</button>
+        <div class="container" style="padding-top: 50px;padding-bottom: 80px;"><button onclick="location.href = 'add.php';" class="btn btn-primary" type="button" style="margin-left: 1006px;margin-bottom: 28px;background-color: rgba(93,130,169,0.83);font-size: 25px;">ADD</button>
             <div class="row">
             <?php foreach ($resultat as $event): ?>
                     
                 <div class="col-md-12" style="padding-top: 20px;padding-bottom: 20px;">
                     <div style="height: 130px;background-color: #ffffff;">
                         <div style="padding-left: 10px; padding-top: 10px;">
-                        <?= $event['titre'] ?> <br> <br> <?= $event['description'] ?>
+                        <id style="font-weight: bold;"> <?= $event['titre'] ?> </id>
+                        <br> <br> 
+                        <?= $event['description'] ?>
                         </div>
                     </div>
                 </div>
@@ -42,10 +44,7 @@ include 'menu.php';
     </div>
     <div class="footer-dark">
 <!---------------php-------------->
-<?php
-include 'footer.php';
-include 'script.php';
-?>
+
 </div><a class="cd-top js-cd-top cd-top--fade-out cd-top--show" style="background-image: url(&quot;assets/img/cd-top-arrow.svg&quot;);background-color: rgba(118,118,118,0.8);background-repeat: no-repeat;" href="#">Top</a>
 
 </body>
