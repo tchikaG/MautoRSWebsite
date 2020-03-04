bf8d34cb52e97a43e81a8e95b59396d1079d1d1f
Dylan Portmann <dylan.portmann@icloud.com>
2020-03-04 12:49:33 +0100

    new

---
+++ shop.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+include 'head.php';
+include 'menu.php';
+
+?>
+
+<body>
+    <div data-bs-parallax-bg="true" style="height: 300px;background-image: url(assets/img/star-sky.jpg);background-position: center;background-size: cover;"></div><i class="fa fa-shopping-basket float-right" style="font-size: 78px;margin-top: 90px;margin-right: 90px;margin-bottom: 50px;margin-left: 30px;"></i>
+    <div class="container" style="height: 1500px;padding-top: 200px;width: 1200px;">
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <div class="pricingTable" style="padding-bottom: 0px;">
+                    <div class="pricingTable-header"><span class="icon"><img style="width: 134px;height: 133px;margin-top: -80px;"></span>
+                        <h1 class="title" style="background-color: rgb(84,149,247);">sticker</h1>
+                        <div class="price-value"><span class="currency" style="margin: -30px;margin-top: -12px;">CHF</span><span>99</span></div>
+                    </div>
+                    <div class="pricing-content">
+                        <ul class="list-unstyled">
+                            <li><strong>1 x </strong>sticker</li>
+                        </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Sign up</a></div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="pricingTable" style="padding-bottom: 0px;">
+                    <div class="pricingTable-header"><span class="icon"><img style="width: 134px;height: 133px;margin-top: -80px;"></span>
+                        <h1 class="title" style="background-color: rgb(84,149,247);">sticker</h1>
+                        <div class="price-value"><span class="currency" style="margin: -30px;margin-top: -12px;">CHF</span><span>99</span></div>
+                    </div>
+                    <div class="pricing-content">
+                        <ul class="list-unstyled">
+                            <li><strong>1 x </strong>sticker</li>
+                        </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Sign up</a></div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 offset-md-3 offset-lg-0">
+                <div class="pricingTable" style="padding-bottom: 0px;">
+                    <div class="pricingTable-header"><span class="icon"><img style="width: 134px;height: 133px;margin-top: -80px;"></span>
+                        <h1 class="title" style="background-color: rgb(84,149,247);">sticker</h1>
+                        <div class="price-value"><span class="currency" style="margin: -30px;margin-top: -12px;">CHF</span><span>99</span></div>
+                    </div>
+                    <div class="pricing-content">
+                        <ul class="list-unstyled">
+                            <li><strong>1 x </strong>sticker</li>
+                        </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Sign up</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <div class="pricingTable" style="padding-bottom: 0px;">
+                    <div class="pricingTable-header"><span class="icon"><img style="width: 134px;height: 133px;margin-top: -80px;"></span>
+                        <h1 class="title" style="background-color: rgb(84,149,247);">sticker</h1>
+                        <div class="price-value"><span class="currency" style="margin: -30px;margin-top: -12px;">CHF</span><span>99</span></div>
+                    </div>
+                    <div class="pricing-content">
+                        <ul class="list-unstyled">
+                            <li><strong>1 x </strong>sticker</li>
+                        </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Sign up</a></div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="pricingTable" style="padding-bottom: 0px;">
+                    <div class="pricingTable-header"><span class="icon"><img style="width: 134px;height: 133px;margin-top: -80px;"></span>
+                        <h1 class="title" style="background-color: rgb(84,149,247);">sticker</h1>
+                        <div class="price-value"><span class="currency" style="margin: -30px;margin-top: -12px;">CHF</span><span>99</span></div>
+                    </div>
+                    <div class="pricing-content">
+                        <ul class="list-unstyled">
+                            <li><strong>1 x </strong>sticker</li>
+                        </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Sign up</a></div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 offset-md-3 offset-lg-0">
+                <div class="pricingTable" style="padding-bottom: 0px;">
+                    <div class="pricingTable-header"><span class="icon"><img style="width: 134px;height: 133px;margin-top: -80px;"></span>
+                        <h1 class="title" style="background-color: rgb(84,149,247);">sticker</h1>
+                        <div class="price-value"><span class="currency" style="margin: -30px;margin-top: -12px;">CHF</span><span>99</span></div>
+                    </div>
+                    <div class="pricing-content">
+                        <ul class="list-unstyled">
+                            <li><strong>1 x </strong>sticker</li>
+                        </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Sign up</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <?php
+include 'footer.php';
+include 'script.php';
+?>
+
+</body>
+
+</html>
