60ff6e1f1f1e65fb089086d4f9997e4b0c7639e2
Dylan Portmann <dylan.portmann@icloud.com>
2020-03-05 08:51:18 +0100

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,3 +1,15 @@
+<?php
+$db_username = 'mautorsadmin';
+$db_password = 'mautorspassword';
+$db_name     = 'mautorsdb';
+$db_host     = 'localhost';
+$db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+       or die('could not connect to database');
+$requete = "SELECT * FROM T_Shop";
+$resultat = $db->query($requete);
+mysqli_fetch_all($resultat,MYSQLI_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -13,19 +25,32 @@ include 'menu.php';
     <link href="pay.php" class="fa fa-shopping-basket float-right" style="font-size: 78px;margin-top: 90px;margin-right: 90px;margin-bottom: 50px;margin-left: 30px;"></link>
     <div class="container" style="height: 1500px;padding-top: 200px;width: 1200px;">
         <div class="row">
+        <?php foreach ($resultat as $shop): ?>
             <div class="col-md-6 col-lg-4">
                 <div class="pricingTable" style="padding-bottom: 0px;">
-                    <div class="pricingTable-header"><span class="icon"><img style="width: 134px;height: 133px;margin-top: -80px;"></span>
-                        <h1 class="title" style="background-color: rgb(84,149,247);">sticker</h1>
-                        <div class="price-value"><span class="currency" style="margin: -30px;margin-top: -12px;">CHF</span><span>99</span></div>
+                    <div class="pricingTable-header"><span class="icon"><img src="" style="width: 134px;height: 133px;margin-top: -80px;"></span>
+                        <h1 class="title" style="background-color: rgb(84,149,247);"><?= $shop['nom'] ?></h1>
+                        <div class="price-value"><span class="currency" style="margin: -30px;margin-top: -12px;">CHF</span><span><?= $shop['prix'] ?></span></div>
                     </div>
                     <div class="pricing-content">
                         <ul class="list-unstyled">
-                            <li><strong>1 x </strong>sticker</li>
-                        </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Sign up</a></div>
+                            <li><strong><?= $shop['nombre'] ?> x </strong><?= $shop['nom'] ?></li>
+                        </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Ajouter</a></div>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-4">
+            <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+    
+    <?php
+include 'footer.php';
+include 'script.php';
+?>
+
+</body>
+<!--
+                <div class="col-md-6 col-lg-4">
                 <div class="pricingTable" style="padding-bottom: 0px;">
                     <div class="pricingTable-header"><span class="icon"><img style="width: 134px;height: 133px;margin-top: -80px;"></span>
                         <h1 class="title" style="background-color: rgb(84,149,247);">sticker</h1>
@@ -86,15 +111,5 @@ include 'menu.php';
                             <li><strong>1 x </strong>sticker</li>
                         </ul><a class="pricingTable-signup" style="background-color: rgb(84,149,247);" href="#">Sign up</a></div>
                 </div>
-            </div>
-        </div>
-    </div>
-    
-    <?php
-include 'footer.php';
-include 'script.php';
-?>
-
-</body>
-
+-->
 </html>
