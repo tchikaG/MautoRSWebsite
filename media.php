9c7124495688ab364912b69e609864bc510ff3b7
Dylan Portmann <44340746+Dyl0nPo@users.noreply.github.com>
2020-03-16 15:15:17 +0100

    Update media.php

--- media.php
+++ media.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 
+
 <?php
 include 'head.php';
 include 'menu.php';
@@ -31,44 +32,104 @@ include 'menu.php';
         <div class="container">
             <h1 class="text-center" style="font-family: Cabin, sans-serif;">Nos PHotos</h1>
             <p class="text-center" style="color: rgb(0,0,0);">Vous pouvez les voirs aussi sur notre <a href="https://www.instagram.com/mautors_/">instagram</a>&nbsp;</p>
-
-
-            <script src="assets/js/InstagramFeed.js"></script>
-<script>
-    (function(){
-        new InstagramFeed({
-            'username': 'mautors_',
-            'container': document.getElementById("instagram-feed1"),
-            'display_profile': true,
-            'display_biography': true,
-            'display_gallery': true,
-            'callback': null,
-            'styling': true,
-            'items': 8,
-            'items_per_row': 4,
-            'margin': 1 
-        });
-    })();
-</script>
-            <!-- <script type="text/javascript" src="path/to/instafeed.min.js"></script>
-            <script type="text/javascript">
-            var feed = new Instafeed({
-            clientId: 'mautors_'
-            });
-            feed.run();
-            </script> -->
-            <div id="instafeed"></div>
-
-            <!-- <div class="powr-instagram-feed" id="d7f498cb_1584011793"></div><script src="https://www.powr.io/powr.js?platform=web"></script> -->
-            <!-- <div class="filtr-controls"><span class="active" data-filter="all">all </span><span data-filter="1">Cars</span><span data-filter="2">Roadtrip</span><span data-filter="3">Landscapes</span></div>
-            <div class="row filtr-container">
-                <div class="col-sm-6 col-md-4 col-lg-3 filtr-item" data-category="1, 3"><a href="https://source.unsplash.com/RLLR0oRz16Y/900x1200.jpg"><img class="img-fluid" data-caption="<strong>MercedesC63 amg</strong><br><em>Roadtrip 1</em>" src="https://source.unsplash.com/RLLR0oRz16Y/600x600.jpg"></a></div>
-                <div class="col-sm-6 col-md-4 col-lg-3 filtr-item"
-                    data-category="2"><a href="https://source.unsplash.com/vUNQaTtZeOo/900x1200.jpg"><img class="img-fluid" src="https://source.unsplash.com/vUNQaTtZeOo/600x600.jpg"></a></div>
-                <div class="col-sm-6 col-md-4 col-lg-3 filtr-item" data-category="1, 3"><a href="https://source.unsplash.com/ZbMJ5VLrpQ4/900x1200.jpg"><img class="img-fluid" src="https://source.unsplash.com/ZbMJ5VLrpQ4/600x600.jpg"></a></div>
-                <div class="col-sm-6 col-md-4 col-lg-3 filtr-item" data-category="2, 3"><a href="https://source.unsplash.com/HWwF4OnXAdM/1200x900.jpg"><img class="img-fluid" src="https://source.unsplash.com/HWwF4OnXAdM/600x600.jpg"></a></div>
-            </div> -->
         </div>
+
+        
+
+        <script src="jquery.instagramFeed.min.js"></script>
+    <script>
+        (function($){
+            $(window).on('load', function(){
+                $.instagramFeed({
+                    'username': 'instagram',
+                    'container': "#instagram-feed1",
+                    'display_profile': true,
+                    'display_biography': true,
+                    'display_gallery': true,
+                    'callback': null,
+                    'styling': true,
+                    'items': 8,
+                    'items_per_row': 4,
+                    'margin': 1
+                }); 
+                
+                $.instagramFeed({
+                    'username': 'github',
+                    'container': "#instagram-feed2",
+                    'display_profile': false,
+                    'display_biography': false,
+                    'display_gallery': true,
+                    'callback': null,
+                    'styling': true,
+                    'items': 8,
+                    'items_per_row': 4,
+                    'margin': 1
+                });
+                $.instagramFeed({
+                    'username': 'zara',
+                    'container': "#instagram-feed3",
+                    'display_profile': false,
+                    'display_biography': false,
+                    'display_gallery': true,
+                    'callback': null,
+                    'styling': true,
+                    'items': 12,
+                    'items_per_row': 6,
+                    'margin': 0.25
+                });
+                $.instagramFeed({
+                    'tag': 'paradise',
+                    'container': "#instagram-feed4",
+                    'display_profile': true,
+                    'display_gallery': true,
+                    'items': 100,
+                    'items_per_row': 5,
+                    'margin': 0.5
+                });
+                $.instagramFeed({
+                    'username': 'fcbarcelona',
+                    'container': "#instagram-feed5",
+                    'display_profile': false,
+                    'display_biography': false,
+                    'display_gallery': false,
+                    'display_igtv': true,
+                    'callback': null,
+                    'styling': true,
+                    'items': 8,
+                    'items_per_row': 4,
+                    'margin': 1
+                });
+                
+                //example 7
+                $.instagramFeed({
+                    'username': 'instagram',
+                    'get_data': true,
+                    'callback': function(data){
+                        $('#jsonHere').html(JSON.stringify(data, null, 2));
+                    }
+                });
+                
+                // Show back to top button if distance from top > 0
+                $(window).scroll(function(){ 
+                    var viewportTop = $(window).scrollTop() + $(window).height();
+             
+                    if($(window).scrollTop() > 10){
+                        $('#btn-top').addClass('d-block');
+                        $('#btn-top').removeClass('d-none');
+                    }else{
+                        $('#btn-top').addClass('d-none');
+                        $('#btn-top').removeClass('d-block');
+                    }
+                });
+
+            });
+        })(jQuery);
+    </script>
+
+
+
+<div id="instagram-feed1" class="instagram_feed"></div>
+
     </section>
     </div><a class="cd-top js-cd-top cd-top--fade-out cd-top--show" style="background-image: url(&quot;assets/img/cd-top-arrow.svg&quot;);background-color: rgba(118,118,118,0.8);background-repeat: no-repeat;" href="#0">Top</a>
     
